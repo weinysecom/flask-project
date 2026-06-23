575249f6cdc571789be3bd6053a61248f892897e
Weiny <secom103152@gmail.com>
2026-06-23 15:36:58 +0800

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__) #Python 內建變數 #__name__ 代表目前使用得模組
 # 被 Python 直接執行的那個檔案，它的 __name__ 會是 __main__
 @app.route("/") #  路由裝飾器（decorator）# 當使用者訪問網站根目錄 / 時，就執行下面這個函式
 def home():
-    return "Hello Flask(2)" #Flask 會把這個字串當成 HTTP response 回傳出去
+    return "Hello Flask(3)" #Flask 會把這個字串當成 HTTP response 回傳出去
 
 @app.route("/test")
 def test():
